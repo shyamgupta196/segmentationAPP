2c75a2b62eb81054a5619f5bc3a1043a5e62fcc5
shyamgupta196 <shyamguptaofficial196@gmail.com>
2021-09-09 06:18:39 +0530

    image still not showing up but it atleast is displaying plot , all i have to do is make it live

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from werkzeug.utils import secure_filename
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.figure import Figure
 import io
+import base64
 from flask import Response
 
 app = Flask(__name__)
@@ -19,23 +20,25 @@ def home():
 @app.route('/images', methods=['POST'])
 def trans():
     if request.method == 'POST':
-        img = request.files['img']
+        img = request.image['img']
         filename = secure_filename(img.filename)
         filepath = os.path.join('templates/uploads/',
                                 filename)
         title = request.values['title']
-
         # print(title)
         plt.savefig(filepath)
         img = reader(filepath)
 
         fig = Figure()
-        axis = fig.add_subplot(1, 1, 1)
-
-        grid = main(img, title)
-        FigureCanvas(fig).print_png(grid)
-        # connecting main with submit
-        return render_template('imagetrans.html', grid=grid)
+        # axis = fig.add_subplot(1, 1, 1)
+        main(img, title)
+        output = io.BytesIO()
+        fig.savefig(output, format='png')
+        # Embed the result in the html output.
+        data = base64.b64encode(output.getbuffer()).decode("ascii")
+        # canvas = FigureCanvas(fig).print_png(grid)
+        return f"<img src='data:image/png;base64,{data}'/>"
+        # connecting main with submit?????
 
 
 if __name__ == '__main__':

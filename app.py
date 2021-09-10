cb7a5abc0f4bdae689b464ad5e9bd6099a35aa93
shyamgupta196 <shyamguptaofficial196@gmail.com>
2021-09-10 18:22:22 +0530

    incomplete image storage linking problem

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def home():
 @app.route('/images', methods=['POST'])
 def trans():
     if request.method == 'POST':
-        img = request.image['img']
+        img = request.files['img']
         filename = secure_filename(img.filename)
         filepath = os.path.join('templates/uploads/',
                                 filename)
@@ -38,7 +38,38 @@ def trans():
         data = base64.b64encode(output.getbuffer()).decode("ascii")
         # canvas = FigureCanvas(fig).print_png(grid)
         return f"<img src='data:image/png;base64,{data}'/>"
-        # connecting main with submit?????
+        # displaying live images from medium article help
+        # saving the images in a db
+
+
+# Matplotlib page
+@app.route('/matplot', methods=("POST", "GET"))
+def mpl():
+    return render_template('imagetrans.html',
+                           PageTitle="Matplotlib")
+
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+
+def create_figure():
+    fig, ax = plt.subplots(figsize=(6, 4))
+    fig.patch.set_facecolor('#E8E5DA')
+
+    x = ECS_data.team
+    y = ECS_data.gw1
+
+    ax.bar(x, y, color="#304C89")
+
+    plt.xticks(rotation=30, size=5)
+    plt.ylabel("Expected Clean Sheets", size=5)
+
+    return fig
 
 
 if __name__ == '__main__':
